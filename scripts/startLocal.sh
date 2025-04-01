1493a170bc9a800332555405d74a2da0f9016190
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-04-01 16:46:39 -0400

    Update start script

--- scripts/startLocal.sh
+++ scripts/startLocal.sh
@@ -2,7 +2,7 @@
 
 
 cd ../../DMVBoardGames
-gnome-terminal -- bash -c "npm run dev; exec bash"
+gnome-terminal -- bash -c "git checkout main; git pull origin main --no-rebase; npm run dev; exec bash"
 
 cd ../backend
 gnome-terminal -- bash -c "mvn clean package; java -jar target/app.jar; exec bash"
