935f404bdcdb2255506301b68584a5de8c0cb659
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-04-17 16:00:05 -0400

    Skip tests when compiling backend

--- scripts/startLocal.sh
+++ scripts/startLocal.sh
@@ -5,4 +5,4 @@ cd ../../DMVBoardGames
 gnome-terminal -- bash -c "git checkout main; git pull origin main --no-rebase; npm run dev; exec bash"
 
 cd ../backend
-gnome-terminal -- bash -c "mvn clean package; java -jar target/app.jar; exec bash"
+gnome-terminal -- bash -c "mvn clean package -DskipTests; java -jar target/app.jar; exec bash"
