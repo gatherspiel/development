80ab44b04636c7bd2ad2d9a9996adac1a6637fd8
Gulu <gulu@createthirdplaces.com>
2025-06-23 19:59:00 -0400

    Create cloneRepos.sh

---
+++ scripts/cloneRepos.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/gatherspiel/api-testing.git
+git clone https://github.com/gatherspiel/database.git
+git clone https://github.com/gatherspiel/backend.git
+git clone https://github.com/gatherspiel/DMVBoardGames.git
